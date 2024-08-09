a25f34a6addb6560439d18c21a043904fa877566
choichangyeon <changyon99@naver.com>
2024-08-10 01:09:57 +0900

    feat-#20 유저정보,지역정보,시간정보 UI구현

--- damyo_app/lib/view/statistics/statistics_view.dart
+++ damyo_app/lib/view/statistics/statistics_view.dart
@@ -1,4 +1,8 @@
+import 'package:damyo_app/style.dart';
+import 'package:damyo_app/widgets/statistics/local_info_widget.dart';
+import 'package:damyo_app/widgets/statistics/timeAver_info_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:damyo_app/widgets/statistics/user_info_widget.dart';
 
 class StatisticsView extends StatefulWidget {
   const StatisticsView({super.key});
@@ -7,9 +11,48 @@ class StatisticsView extends StatefulWidget {
   State<StatisticsView> createState() => _StatisticsViewState();
 }
 
-class _StatisticsViewState extends State<StatisticsView> {
+class _StatisticsViewState extends State<StatisticsView> with SingleTickerProviderStateMixin{
+  late TabController _tabController;
+  bool timeCheck = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);  
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();  
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Statistics");
+    return Scaffold(
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+        backgroundColor: Colors.white,
+        title: textFormat(text: '통계', fontSize: 20),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            userInfo(context),
+            SizedBox(height: 20,),
+            localInfo(context, _tabController),
+            timeAverInfo(context, timeCheck, (newValue){
+              setState(() {
+                timeCheck = newValue;
+              });
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
