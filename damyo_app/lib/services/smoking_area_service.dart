18d1f29ec578c534846014bfba09c5cc1a26f841
kevinmj12 <kevinmj12@gmail.com>
2024-08-10 22:10:37 +0900

    feat:#24-제보API, 리뷰API 연동 완료

--- damyo_app/lib/services/smoking_area_service.dart
+++ damyo_app/lib/services/smoking_area_service.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:damyo_app/models/smoking_area/sa_basic_model.dart';
 import 'package:damyo_app/models/smoking_area/sa_detail_model.dart';
 import 'package:damyo_app/models/smoking_area/sa_inform_model.dart';
+import 'package:damyo_app/models/smoking_area/sa_review_model.dart';
 import 'package:damyo_app/models/smoking_area/sa_search_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -61,9 +62,6 @@ class SmokingAreaService {
 
     // 이미지 추가
     if (image != null) {
-      var imageFile = File(image.path);
-      var stream = http.ByteStream(imageFile.openRead());
-      var length = await imageFile.length();
       var multipartFile = await http.MultipartFile.fromPath(
         'imgFile',
         image.path,
@@ -77,6 +75,53 @@ class SmokingAreaService {
     List<int> jsonData = utf8.encode(jsonEncode(data));
     request.files.add(http.MultipartFile.fromBytes(
       'areaRequest',
+      jsonData,
+      contentType: MediaType(
+        'application',
+        'json',
+        {'charset': 'utf-8'},
+      ),
+    ));
+
+    var response = await request.send();
+    var jsonBody = await response.stream.bytesToString();
+
+    if (response.statusCode == 200) {
+      debugPrint(jsonBody);
+      return true;
+    } else {
+      debugPrint(jsonBody);
+      return false;
+    }
+  }
+
+  static Future<bool> reviewSmokingArea(
+      XFile? image, SaReviewModel saReviewModel) async {
+    final baseUrl = dotenv.get('BASE_URL');
+    var url = Uri.parse('$baseUrl/info/postInfo');
+
+    var request = http.MultipartRequest('POST', url);
+
+    // 토큰 추가
+    String token = "";
+    request.headers['Authorization'] = 'Bearer $token';
+
+    // 이미지 추가
+    if (image != null) {
+      var multipartFile = await http.MultipartFile.fromPath(
+        'imgFile',
+        image.path,
+        filename: 'image.png',
+      );
+      request.files.add(multipartFile);
+    }
+
+    // 데이터 추가
+    var data = saReviewModel.toJson();
+    debugPrint(data["areaId"]);
+    List<int> jsonData = utf8.encode(jsonEncode(data));
+    request.files.add(http.MultipartFile.fromBytes(
+      'updateInfoRequest',
       jsonData,
       contentType: MediaType(
         'application',
