7e9abb0b0e942d462d287f857798a21edbea142e
kevinmj12 <kevinmj12@gmail.com>
2024-08-16 18:30:34 +0900

    feat:#29-최근검색어 저장 및 불러오기 구현 완료

--- damyo_app/lib/view_models/map_models/search/sa_search_view_model.dart
+++ damyo_app/lib/view_models/map_models/search/sa_search_view_model.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:core';
+
 import 'package:damyo_app/models/smoking_area/sa_basic_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SaSearchViewModel extends ChangeNotifier {
   // 상태
@@ -15,7 +19,7 @@ class SaSearchViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  // 흡연구역 검색 내역
+  // 흡연구역 검색 결과
   List<SaBasicModel> _searchedSaList = [];
   List<SaBasicModel> get searchedSaList => _searchedSaList;
 
@@ -24,7 +28,55 @@ class SaSearchViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
+  // 최근검색어 rhksfus
+  bool _updateRecentWords = false;
+  bool get updateRecentWords => _updateRecentWords;
+
+  List<String> _recentSearchWords = [];
+  List<String> get recentSearchWords => _recentSearchWords;
+  List<String> _recentSearchDates = [];
+  List<String> get recentSearchDates => _recentSearchDates;
+
+  readRecentSearchWords() async {
+    if (_updateRecentWords == false) {
+      FlutterSecureStorage storage = const FlutterSecureStorage();
+      String? strWords = await storage.read(key: "recentSearchWords");
+      if (strWords == null) {
+        _recentSearchWords = [];
+      } else {
+        _recentSearchWords = jsonDecode(strWords).map((item) {
+          return item
+              .map((key, value) => MapEntry(key.toString(), value.toString()));
+        }).toList();
+      }
+      String? strDates = await storage.read(key: "recentSearchDates");
+      if (strDates == null) {
+        _recentSearchDates = [];
+      } else {
+        _recentSearchDates = jsonDecode(strDates).map((item) {
+          return item
+              .map((key, value) => MapEntry(key.toString(), value.toString()));
+        }).toList();
+      }
+
+      _updateRecentWords = true;
+    }
+  }
+
+  updateRecentSearchWord(String word) async {
+    if (_recentSearchWords.contains(word)) {}
+    _recentSearchWords.add(word);
+    _recentSearchDates.add(DateTime.now().toString().substring(5, 10));
+    FlutterSecureStorage storage = const FlutterSecureStorage();
+    await storage.write(
+        key: "recentSearchWords", value: jsonEncode(_recentSearchWords));
+    await storage.write(
+        key: "recentSearchDates", value: jsonEncode(_recentSearchDates));
+
+    notifyListeners();
+  }
+
   // TextEditingController
-  final TextEditingController _saNameController = TextEditingController();
-  TextEditingController get saNameController => _saNameController;
+  final TextEditingController _searchWordController = TextEditingController();
+  TextEditingController get searchWordController => _searchWordController;
 }
