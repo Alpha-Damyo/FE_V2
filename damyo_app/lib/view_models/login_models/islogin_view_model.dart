c6507aa50700513e1ec0bf2e3d22b082e89089b4
choichangyeon <changyon99@naver.com>
2024-08-18 15:20:15 +0900

    feat-#20 로그인 로직 및 secureStorage연결

--- damyo_app/lib/view_models/login_models/islogin_view_model.dart
+++ damyo_app/lib/view_models/login_models/islogin_view_model.dart
@@ -1,19 +1,23 @@
 import 'package:damyo_app/models/login/islogin_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class IsloginViewModel extends ChangeNotifier {
-  
+  FlutterSecureStorage storage = FlutterSecureStorage();
+
   final IsloginModel _isloginModel = IsloginModel(isLogin: false);
   
   IsloginModel get isloginModel => _isloginModel;
 
   void login() {
     _isloginModel.isLogin = true;
+    storage.write(key: 'loginState', value: 'true');
     notifyListeners();
   }
 
   void logout() async {
     _isloginModel.isLogin = false;
+    storage.write(key: 'loginState', value: 'false');
     notifyListeners();
   }
 }
