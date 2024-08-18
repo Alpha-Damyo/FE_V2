891886b7b0bb9776fcad1bee45d1c7180349e3fa
kevinmj12 <kevinmj12@gmail.com>
2024-08-18 16:45:17 +0900

    feat:#29-검색 결과 지도 화면 최적화

--- damyo_app/lib/view_models/map_models/search/sa_search_view_model.dart
+++ damyo_app/lib/view_models/map_models/search/sa_search_view_model.dart
@@ -2,8 +2,9 @@ import 'dart:convert';
 import 'dart:core';
 
 import 'package:damyo_app/models/smoking_area/sa_basic_model.dart';
+import 'package:damyo_app/style.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SaSearchViewModel extends ChangeNotifier {
@@ -23,12 +24,83 @@ class SaSearchViewModel extends ChangeNotifier {
   List<SaBasicModel> _searchedSaList = [];
   List<SaBasicModel> get searchedSaList => _searchedSaList;
 
-  setSearchedSaList(List<SaBasicModel> newList) {
+  setSearchedSaList(BuildContext context, List<SaBasicModel> newList) async {
+    // 검색된 리스트 반영
     _searchedSaList = newList;
+    // 지도에도 반영하여 마커 추가
+    _naverMapController.clearOverlays();
+    const basicIcon = Icon(
+      Icons.location_on,
+      color: Colors.red,
+      size: 40,
+    );
+    for (int i = 0; i < newList.length; i++) {
+      final iconImage = await NOverlayImage.fromWidget(
+          widget: Column(
+            children: [
+              basicIcon,
+              Stack(
+                children: [
+                  Text(
+                    // 디바이스의 글자 스케일 등을 처리할 때 이용
+                    _searchedSaList[i].name,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      foreground: Paint()
+                        ..color = Colors.white
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 2,
+                    ),
+                    // 텍스트의 오버플로우 처리가 있을 경우 동시에 적용
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(_searchedSaList[i].name,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        overflow: TextOverflow.ellipsis,
+                        color: Colors.black,
+                      )),
+                ],
+              )
+            ],
+          ),
+          size: const Size(140, 70),
+          context: context);
+
+      final NMarker marker = NMarker(
+        id: _searchedSaList[i].areaId,
+        position: NLatLng(
+          _searchedSaList[i].latitude,
+          _searchedSaList[i].longitude,
+        ),
+        icon: iconImage,
+      );
+
+      marker.setOnTapListener((overlay) {
+        updateSearchSelectedSa(_searchedSaList[i]);
+      });
+      // marker.setZIndex(i);
+      marker.setMinZoom(12);
+      marker.setIsMinZoomInclusive(true);
+      marker.setIsMaxZoomInclusive(false);
+      _naverMapController.addOverlay(marker);
+
+      // final onMarkerInfoWindow = NInfoWindow.onMarker(
+      //   id: marker.info.id,
+      //   text: _searchedSaList[i].name,
+      // );
+      // marker.openInfoWindow(onMarkerInfoWindow);
+
+      // final infoWindow = NInfoWindow.onMarker(
+      //     id: marker.info.id, text: _searchedSaList[i].name);
+      // _naverMapController.addOverlay(infoWindow);
+    }
     notifyListeners();
   }
 
-  // 최근검색어 rhksfus
+  // 최근검색어 관련
   bool _updateRecentWords = false;
   bool get updateRecentWords => _updateRecentWords;
 
@@ -90,4 +162,29 @@ class SaSearchViewModel extends ChangeNotifier {
   // TextEditingController
   final TextEditingController _searchWordController = TextEditingController();
   TextEditingController get searchWordController => _searchWordController;
+
+  // 지도에서 검색된 흡연구역을 보여줌
+  SaBasicModel _searchSelectedSa =
+      SaBasicModel("null", "null", 37.5666, 126.979, "null", 0);
+  SaBasicModel get searchSelectedSa => _searchSelectedSa;
+
+  updateSearchSelectedSa(SaBasicModel sa) {
+    _searchSelectedSa = sa;
+    notifyListeners();
+  }
+
+  late NaverMapController _naverMapController;
+  updateNaverMapController(NaverMapController controller) {
+    _naverMapController = controller;
+  }
+
+  moveMapCamera() {
+    _naverMapController.updateCamera(
+      NCameraUpdate.scrollAndZoomTo(
+        target:
+            NLatLng(_searchSelectedSa.latitude, _searchSelectedSa.longitude),
+        zoom: 15,
+      ),
+    );
+  }
 }
