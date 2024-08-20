f5852c32ae40ec27c15a3a88983aa46ba5dc9c8f
choichangyeon <changyon99@naver.com>
2024-08-20 22:03:54 +0900

    fix-#17 로그인 흐름 변경

--- damyo_app/lib/services/naverlogin_service.dart
+++ damyo_app/lib/services/naverlogin_service.dart
@@ -1,11 +1,12 @@
 import 'package:damyo_app/services/login_state_check.dart';
 import 'package:damyo_app/view_models/login_models/islogin_view_model.dart';
 import 'package:damyo_app/view_models/login_models/token_view_model.dart';
+import 'package:damyo_app/view_models/login_models/user_info_view_model.dart';
 import 'package:flutter_naver_login/flutter_naver_login.dart';
 import 'package:damyo_app/services/login_service.dart';
 
 Future<int> signInWithNaver(
-    IsloginViewModel isloginViewModel, TokenViewModel tokenViewModel) async {
+    IsloginViewModel isloginViewModel, TokenViewModel tokenViewModel, UserInfoViewModel userInfoViewModel) async {
   NaverLoginResult naverUser = await FlutterNaverLogin.logIn();
   NaverAccessToken naverToken = await FlutterNaverLogin.currentAccessToken;
   Map<String, dynamic> userInfo;
@@ -19,6 +20,8 @@ Future<int> signInWithNaver(
   userInfo = await login({
     "token": naverToken.accessToken,
   }, "naver");
+
+  userInfoViewModel.setName(naverUser.account.name);
 
   return await checkLoginState(userInfo, isloginViewModel, tokenViewModel);
 }
