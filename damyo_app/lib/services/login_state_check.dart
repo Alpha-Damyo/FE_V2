5bfb72bb2a553738bafdd0eb3f27af56886d7e3d
kevinmj12 <kevinmj12@gmail.com>
2024-08-20 22:01:15 +0900

    feat:#38-구글로그인 구현 완료

--- damyo_app/lib/services/login_state_check.dart
+++ damyo_app/lib/services/login_state_check.dart
@@ -1,23 +1,19 @@
 import 'package:damyo_app/view_models/login_models/islogin_view_model.dart';
 import 'package:damyo_app/view_models/login_models/token_view_model.dart';
-import 'package:damyo_app/view_models/login_models/user_info_view_model.dart';
 
-Future<int> checkLoginState(
-      Map<String, dynamic> userInfo, IsloginViewModel isloginViewModel, TokenViewModel tokenViewModel) async {
-    
-    if (userInfo['code'] == "A102") {
-      // 회원 가입 필요
-      return 1;
-    }
-    else if(userInfo['code'] == "A103"){
-      // 토큰 재발급 필요
-      return 2;
-    } 
-    else {
-      // 로그인 성공, 토큰 저장
-      tokenViewModel.setToken(userInfo['accessToken'], userInfo['refreshToken']);
-      isloginViewModel.login();
+Future<int> checkLoginState(Map<String, dynamic> userInfo,
+    IsloginViewModel isloginViewModel, TokenViewModel tokenViewModel) async {
+  if (userInfo['code'] == "A102") {
+    // 회원 가입 필요
+    return 1;
+  } else if (userInfo['code'] == "A103") {
+    // 토큰 재발급 필요
+    return 2;
+  } else {
+    // 로그인 성공, 토큰 저장
+    tokenViewModel.setToken(userInfo['accessToken'], userInfo['refreshToken']);
+    isloginViewModel.login();
 
-      return 0;
-    }
+    return 0;
   }
+}
