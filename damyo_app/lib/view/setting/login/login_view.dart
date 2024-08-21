aa6124845fd0a288bdb043a2c8826a552dda5e7c
kevinmj12 <kevinmj12@gmail.com>
2024-08-22 02:43:28 +0900

    feat:#38-자동로그인구현완료

--- damyo_app/lib/view/setting/login/login_view.dart
+++ damyo_app/lib/view/setting/login/login_view.dart
@@ -1,5 +1,5 @@
 import 'package:damyo_app/style.dart';
-import 'package:damyo_app/view_models/login_models/islogin_view_model.dart';
+import 'package:damyo_app/view_models/login_models/is_login_view_model.dart';
 import 'package:damyo_app/view_models/login_models/token_view_model.dart';
 import 'package:damyo_app/view_models/login_models/user_info_view_model.dart';
 import 'package:flutter/material.dart';
--- damyo_app/lib/widgets/setting_widgets.dart
+++ damyo_app/lib/widgets/setting_widgets.dart
@@ -1,5 +1,6 @@
 import "package:damyo_app/view/setting/login/login_view.dart";
-import "package:damyo_app/view_models/login_models/islogin_view_model.dart";
+import "package:damyo_app/view_models/login_models/is_login_view_model.dart";
+import "package:damyo_app/view_models/login_models/token_view_model.dart";
 import "package:damyo_app/view_models/login_models/user_info_view_model.dart";
 import "package:flutter/material.dart";
 import "package:damyo_app/style.dart";
@@ -41,38 +42,26 @@ Widget loginBtn(BuildContext context) {
 
 // 사용자 기본 정보
 Widget userProfile(BuildContext context, IsloginViewModel isloginViewModel,
-    UserInfoViewModel userInfoViewModel) {
+    TokenViewModel tokenViewModel, UserInfoViewModel userInfoViewModel) {
   return Container(
     width: double.infinity,
     padding: const EdgeInsets.all(15),
     child: Column(
       children: [
         Row(
-          mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
               children: [
                 Container(
-                  decoration: const BoxDecoration(
-                      color: Color(0xFFDEDEDE), shape: BoxShape.circle),
-                  child: const Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: 84,
-                        height: 84,
-                        // decoration: BoxDecoration(
-                        //   image: DecorationImage(
-                        //     image:
-                        //         NetworkImage('no'),
-                        //     fit: BoxFit.fill,
-                        //   ),
-                        // ),
-                      ),
-                    ],
+                  width: 84,
+                  height: 84,
+                  decoration: const BoxDecoration(shape: BoxShape.circle),
+                  child: ClipOval(
+                    child: Image.network(
+                      userInfoViewModel.userInfoModel.profileUrl,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
                 const SizedBox(width: 15),
@@ -99,14 +88,14 @@ Widget userProfile(BuildContext context, IsloginViewModel isloginViewModel,
                           borderRadius: BorderRadius.circular(5),
                         ),
                       ),
-                      child: const Row(
+                      child: Row(
                         mainAxisSize: MainAxisSize.min,
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
                           Text(
-                            '바른 문화 기여도: 0점',
-                            style: TextStyle(
+                            '바른 문화 기여도: ${userInfoViewModel.userInfoModel.contribution}점',
+                            style: const TextStyle(
                               color: Colors.black,
                               fontSize: 12,
                               fontFamily: 'Pretendard',
@@ -122,7 +111,7 @@ Widget userProfile(BuildContext context, IsloginViewModel isloginViewModel,
             ),
             IconButton(
               onPressed: () {
-                print(isloginViewModel.isloginModel.isLogin);
+                print(isloginViewModel.isLogin);
               },
               icon: const Icon(Icons.keyboard_arrow_right),
               iconSize: 30,
@@ -133,6 +122,7 @@ Widget userProfile(BuildContext context, IsloginViewModel isloginViewModel,
         InkWell(
           borderRadius: BorderRadius.circular(10),
           onTap: () {
+            tokenViewModel.deleteToken();
             isloginViewModel.logout();
           },
           child: Ink(
@@ -154,48 +144,21 @@ Widget userProfile(BuildContext context, IsloginViewModel isloginViewModel,
 
 // 설정 탭 박스
 Widget toolBax(BuildContext context, String name) {
-  return Container(
-    width: double.infinity,
-    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          width: double.infinity,
-          height: 30,
-          child: InkWell(
-            onTap: () {},
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: textFormat(
-                  text: name,
-                  color: const Color(0xFF262B32),
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ),
+  return InkWell(
+    onTap: () {},
+    child: Ink(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      width: double.infinity,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 15),
+        decoration: const BoxDecoration(
+            border: Border(bottom: BorderSide(width: 1.5, color: Colors.grey))),
+        child: textFormat(
+          text: name,
+          color: const Color(0xFF262B32),
+          fontSize: 16,
         ),
-        const SizedBox(
-          height: 20,
-        ),
-        Container(
-          width: double.infinity,
-          decoration: const ShapeDecoration(
-            shape: RoundedRectangleBorder(
-              side: BorderSide(
-                width: 1,
-                strokeAlign: BorderSide.strokeAlignCenter,
-                color: Color(0xFFEEF1F4),
-              ),
-            ),
-          ),
-        )
-      ],
+      ),
     ),
   );
 }
