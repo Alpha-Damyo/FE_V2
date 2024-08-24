848371e6a55186fcee904baaa6ad0fdb60ea0663
choichangyeon <changyon99@naver.com>
2024-08-24 20:40:34 +0900

    feat-#54 흡연완료데이터 서버전송 구현

--- damyo_app/lib/view_models/statistics_models/locaI_info_view_model.dart
+++ damyo_app/lib/view_models/statistics_models/locaI_info_view_model.dart
@@ -2,12 +2,13 @@ import 'package:damyo_app/database/smoke_data.dart';
 import 'package:damyo_app/models/smoking_area/sa_basic_model.dart';
 import 'package:damyo_app/models/statistics_info/stat_region_model.dart';
 import 'package:damyo_app/services/statistics_service.dart';
-import 'package:damyo_app/services/smoking_area_service.dart';import 'package:flutter/material.dart';
+import 'package:damyo_app/services/smoking_area_service.dart';
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 
 class LocalInfoViewModel extends ChangeNotifier {
   LocalInfoViewModel();
-  
+
   List<dynamic>? _GuList = [];
   List<dynamic>? _areaList = [];
   List<dynamic>? _areaInfo = [];
@@ -15,28 +16,40 @@ class LocalInfoViewModel extends ChangeNotifier {
   List<dynamic>? get GuList => _GuList;
   List<dynamic>? get areaList => _areaList;
   List<dynamic>? get areaInfo => _areaInfo;
-  
-  Future<void> fetchLocalDB()async{
+
+  Future<void> fetchLocalDB() async {
     List<SaBasicModel>? area = [];
-    
+
     statRegionModel? Region;
 
     Region = await getRegionStatics();
 
     _areaList = Region.areaTop;
+    List<dynamic>? _gulist = Region.allRegion;
 
     if (_areaList != null) {
       for (int i = 0; i < _areaList!.length; i++) {
+        if (i >= 3) {
+          break;
+        }
         SaBasicModel popArea =
-            await SmokingAreaService.serachSmokingAreaByAreaId(_areaList![i].keys.first);
+            await SmokingAreaService.serachSmokingAreaByAreaId(
+                _areaList![i].keys.first);
         area.add(popArea);
       }
     }
 
-    _GuList = Region.allRegion;
-    _areaInfo = area;
+    if (_gulist != null) {
+      for (int i = 0; i < _gulist.length; i++) {
+        if (i >= 3) {
+          break;
+        }
+        _GuList?.add(_gulist[i]);
+      }
+    }
     
+    _areaInfo = area;
+
     notifyListeners();
   }
 }
-
