969fa5f5fb34b7bd2fcb477228d1e040d223e20b
choichangyeon <changyon99@naver.com>
2024-08-28 20:49:08 +0900

    refactor-#54 권한 설정 수정

--- damyo_app/lib/utils/get_permission.dart
+++ damyo_app/lib/utils/get_permission.dart
@@ -8,7 +8,6 @@ Future<void> getPhotoPermission() async {
 // 갤러리 권한 조회
 Future<bool> checkPhotoPermission() async {
   var status = await Permission.photos.isGranted;
-  
   if (status == true) {
     return Future.value(true);
   } else {
